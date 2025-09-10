28e0241b7fdd457ad2d729edd778894e3c30e40c
Dana <danamushchenko@gmail.com>
2025-09-10 22:48:31 +0300

    Skeleton project

---
+++ src/test/java/com/example/athleticore/AthletiCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.athleticore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthletiCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
