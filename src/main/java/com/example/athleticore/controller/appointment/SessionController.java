122465070fcb074fbbb7ad7f5c2bbc45c2663c4a
Dana <danamushchenko@gmail.com>
2025-09-21 17:52:22 +0300

    add SessionController and validation for entity

--- src/main/java/com/example/athleticore/controller/appointment/SessionController.java
+++ src/main/java/com/example/athleticore/controller/appointment/SessionController.java
@@ -1,39 +1,40 @@
 package com.example.athleticore.controller.appointment;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
+import com.example.athleticore.dto.sessions.SessionDto;
+import com.example.athleticore.dto.sessions.UpdateSessionFields;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/session")
 public class SessionController {
+    @GetMapping("/{idUser}")
+    public void getSessionByUser(@PathVariable Long idUser){
+        //return session.getVyId();
+    }
 
     //permission - manager(create new repeat session and connects trainer)
     @PostMapping("/")
-    public void createSession(){
+    public void createSession(@Valid @RequestBody SessionDto sessionDto){
         //send notification to trainer to accept(optional)
     }
 
-
     //permission - manager(create new repeat session and connects trainer)
-    @PostMapping("/update")
-    public void updateSession(){
+    @PatchMapping("/{id}")
+    public void updateSession(@PathVariable Long id, @RequestBody UpdateSessionFields updateSessionFields){
 
     }
-
 
     //permission - manager(create new repeat session and connects trainer)
     @DeleteMapping("/")
     public void deleteSession(){
 
-    }
-
-    @GetMapping("/")
-    public void showAllSession(){
-
-    }
-
-    @GetMapping("/{idUser}")
-    public void showSessionByUser(@PathVariable Long idUser){
-        //return session.getVyId();
     }
 }
---
+++ src/main/java/com/example/athleticore/dto/sessions/UpdateSessionFields.java
@@ -0,0 +1,4 @@
+package com.example.athleticore.dto.sessions;
+
+public class UpdateSessionFields {
+}
