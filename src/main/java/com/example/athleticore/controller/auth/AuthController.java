e3ad9866cebe2d1bf54c6effa06c1be78981b0ef
yelyzavetaklymenko <lizaklim2005@gmail.com>
2025-09-24 00:19:51 +0300

    added controller for login with validation and main page

--- src/main/java/com/example/athleticore/controller/auth/AuthController.java
+++ src/main/java/com/example/athleticore/controller/auth/AuthController.java
@@ -1,19 +1,21 @@
 package com.example.athleticore.controller.auth;
 
+import com.example.athleticore.dto.auth.CredentialDto;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/auth")
 public class AuthController {
     @GetMapping("/login")
-    public void login(){
+    public void login(@Valid @RequestBody CredentialDto credentialDto){
 
     }
 
-    //only manager has permision
     @PostMapping("/register")
     public void register(){
 
---
+++ src/main/java/com/example/athleticore/exception/user/DeletingTrainerException.java
@@ -0,0 +1,7 @@
+package com.example.athleticore.exception.user;
+
+public class DeletingTrainerException extends RuntimeException{
+    public DeletingTrainerException(String message) {
+        super(message);
+    }
+}
