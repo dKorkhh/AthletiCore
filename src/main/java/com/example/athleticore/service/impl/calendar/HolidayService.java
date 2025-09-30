05555ca2cf6cf9ccb77b41e19579614a24ecbf64
Dana <danamushchenko@gmail.com>
2025-09-30 21:35:22 +0300

    add bean for restTemplate and notification logic

--- src/main/java/com/example/athleticore/service/impl/calendar/HolidayService.java
+++ src/main/java/com/example/athleticore/service/impl/calendar/HolidayService.java
@@ -2,6 +2,7 @@ package com.example.athleticore.service.impl.calendar;
 
 import com.example.athleticore.dto.calendar.HolidayDto;
 import com.example.athleticore.exception.data.CallApiException;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -11,14 +12,19 @@ import java.util.Arrays;
 import java.util.List;
 
 @Service
+@Setter
 public class HolidayService {
-    private final RestTemplate restTemplate = new RestTemplate();
+    private RestTemplate restTemplate;
 
     @Value("${spring.application.holiday-api.url}")
     private String HOLIDAY_API_URL;
 
     @Value("${spring.application.holiday-api.region}")
     private String region;
+
+    public HolidayService(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
 
     public List<HolidayDto> getListOfHolidays() {
         int currentYear = java.time.Year.now().getValue();
