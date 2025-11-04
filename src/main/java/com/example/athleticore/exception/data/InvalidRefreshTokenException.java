cc3166273fe164fd770543433a71fbad1704cf98
Oryna Zudilova <zarinamcnulty@gmail.com>
2025-11-04 22:21:49 +0200

    add login and registration

--- src/main/java/com/example/athleticore/exception/data/InvalidRefreshTokenException.java
+++
@@ -1,7 +0,0 @@
-package com.example.athleticore.exception.data;
-
-public class InvalidRefreshTokenException extends RuntimeException {
-    public InvalidRefreshTokenException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/example/athleticore/exception/user/DeletingTrainerException.java
+++
@@ -1,7 +0,0 @@
-package com.example.athleticore.exception.user;
-
-public class DeletingTrainerException extends RuntimeException{
-    public DeletingTrainerException(String message) {
-        super(message);
-    }
-}
