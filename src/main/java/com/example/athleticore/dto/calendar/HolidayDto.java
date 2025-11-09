faeeb89144bad904802f9a05f1763ed36ac23cc8
dana <danamuschenko@gmail.com>
2025-11-09 23:56:32 +0200

    Added test conf

--- src/main/java/com/example/athleticore/dto/calendar/HolidayDto.java
+++ src/main/java/com/example/athleticore/dto/calendar/HolidayDto.java
@@ -1,8 +1,10 @@
 package com.example.athleticore.dto.calendar;
 
+import lombok.Builder;
 import lombok.Data;
 
 @Data
+@Builder
 public class HolidayDto {
     private String date;
     private String localName;
--- src/main/java/com/example/athleticore/dto/sessions/SessionDto.java
+++ src/main/java/com/example/athleticore/dto/sessions/SessionDto.java
@@ -7,11 +7,13 @@ import com.example.athleticore.enums.SessionType;
 import com.example.athleticore.utils.validation.DateIsNotHolidayValidator;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
+import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
 
 @Data
+@Builder
 public class SessionDto {
     @NotNull
     @Size(max = 12)
