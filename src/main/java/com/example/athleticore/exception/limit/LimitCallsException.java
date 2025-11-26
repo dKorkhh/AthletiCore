462b0f2c308c199b2243cde4d83c395bf733395a
Oryna Zudilova <zarinamcnulty@gmail.com>
2025-11-26 23:41:06 +0100

    Added cache

--- src/main/java/com/example/athleticore/exception/limit/LimitCallsException.java
+++
@@ -1,7 +0,0 @@
-package com.example.athleticore.exception.limit;
-
-public class LimitCallsException extends RuntimeException {
-    public LimitCallsException(String message) {
-        super(message, null, false, false);
-    }
-}
--- src/main/java/com/example/athleticore/repository/ScheduleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.athleticore.repository;
-
-import com.example.athleticore.entity.Schedule;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
-}
