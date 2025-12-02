5c15375378db5e0c5bb6d6d78737c12a3f82b2c1
yelyzavetaklymenko <lizaklim2005@gmail.com>
2025-12-02 19:15:28 +0200

    prepare for job

--- src/main/java/com/example/athleticore/service/BookingService.java
+++ src/main/java/com/example/athleticore/service/BookingService.java
@@ -3,7 +3,9 @@ package com.example.athleticore.service;
 import com.example.athleticore.dto.bookings.BookingDto;
 import com.example.athleticore.dto.bookings.BookingResponseDTO;
 import com.example.athleticore.entity.Booking;
+import com.example.athleticore.enums.BookingStatus;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface BookingService {
@@ -12,5 +14,13 @@ public interface BookingService {
     public List<Booking> getAllBooking();
     List<Booking> findBookingsByCurrentUser();
     List<Booking> findBookingsBySessionId(Long id);
+    void deleteBookingBySessionId(Long id);
+
     List<Booking> getAllBookingWithSession();
+
+    List<Booking> findExpiredBookings(List<BookingStatus> statuses, LocalDateTime threshold);
+
+    List<Booking> findAllById(List<Long> bookingIds);
+
+    void saveAll(List<Booking> expired);
 }
